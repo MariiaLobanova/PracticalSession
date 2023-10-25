362792e576813db9f72b8944930dbc55fb68aa03
Lobanova_Mariia <lobanovmariia@gmail.com>
2023-10-25 06:34:28 +0200

    added E5

---
+++ Class2410/E5.java
@@ -0,0 +1,25 @@
+package org.example.Class2410;
+
+import java.io.*;
+import java.util.Scanner;
+
+// Implement a feature that allows the user to specify the name of the
+//file where their data will be stored and read from.
+public class E5 {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        System.out.print("Enter file name to read/write data: ");
+        String fileName = scanner.nextLine();
+
+       try{ BufferedReader reader = new BufferedReader(new FileReader(fileName));
+               String line;
+               while((line= reader.readLine())!=null){
+                   System.out.println(line);
+               }
+               reader.close();
+
+    } catch (IOException e) {
+           System.out.println(e.getMessage());
+       }
+    }
+}
